5ff6ed3f580ccf4aa4d062fbce5c3bbb3345affa
Dechen Zeng <54780395+zengdechen@users.noreply.github.com>
2022-09-19 00:23:09 -0400

    update graphics

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,7 +8,7 @@ function TopBar(props) {
     return (
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <span className="App-title">Around Web</span>
+            <span className="App-title">InstaPost</span>
             {
                 isLoggedIn ?
                     <LogoutOutlined className='logout' onClick={handleLogout}/>
